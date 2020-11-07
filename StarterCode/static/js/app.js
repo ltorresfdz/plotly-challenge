1170f25ff55fd63252908a2b2d77b8b2b660b567
Laura Torres <ltorresfdz@gmail.com>
2020-11-07 17:22:45 -0600

    reading json file ok

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,20 @@
+d3.json("samples.json").then((data) => {
+    console.log(data);
+var metadata = data.metadata;
+var samples= data.samples;
+var names=data.names;
+console.log("uno")
+
+
+// console.log(metadata);
+// console.log(names);
+// console.log(samples);
+
+
+
+});
+
+
+
+
+
