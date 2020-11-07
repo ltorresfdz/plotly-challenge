a03e0fb81f10ae66480b81edd6307e2a1ac511a9
Laura Torres <ltorresfdz@gmail.com>
2020-11-07 17:42:16 -0600

    assigned the test id values to selector

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -5,14 +5,26 @@ var samples= data.samples;
 var names=data.names;
 console.log("uno")
 
-
 // console.log(metadata);
 // console.log(names);
 // console.log(samples);
-
-
-
 });
+
+//assigns the test subject ID No. to the selector:
+var option = d3.select("#selDataset");
+d3.json("samples.json").then((data) => {
+var sampleName= data.names;
+sampleName.forEach((sample)=> {
+  option.append("option").text(sample).property("value",sample);
+})
+    
+});
+
+
+
+
+
+
 
 
 
